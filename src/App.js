5acc658d0a28f0ea07dcb945851370476345b453
Deeya <guptadeeya12@gmail.com>
2023-04-28 10:02:06 +0530

    Making State persist on refresh-1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
         <>
         {/* AnimatePresence make sure that all the animations are under this app.js */}
-        <AnimatePresence>
+        <AnimatePresence mode='wait'>
             <div className='w-screen h-auto flex flex-col bg-primary'>
                 <Header />
                 <main className='mt-24 p-8 w-full'>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../img/logo.png'
-import { MdShoppingBasket } from "react-icons/md"
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md"
 import Avatar from '../img/avatar.png'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
@@ -14,15 +14,26 @@ const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const [{user}, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue()
+
+  const [isMenu, setIsMenu] = useState(false);
 
   const login = async () => {
-    const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider)
-    // console.log(response);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0]
-    })
+    if (!user) {
+      // Below comment is nothing but used to ignore the warning that is caused by refreshToken in this
+      // eslint-disable-next-line
+      const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider)
+      // console.log(response);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      })
+
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
+    else {
+      setIsMenu(!isMenu)
+    }
   };
 
   return (
@@ -39,14 +50,18 @@ const Header = () => {
         </Link>
         <div className='flex items-center gap-8'>
           {/* ml-auto is classname used for marginleft-auto which pushes the items to the right side */}
-          <ul className='flex items-center gap-10'>
+          <motion.ul 
+          initial={{opacity: 0, x:200}}
+          animate={{opacity:1, x:0}}
+          exit={{opacity:0, x:200}}
+          className='flex items-center gap-10'>
 
             {/* text-base is 16px */}
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in ease-out cursor-pointer'>Home</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in ease-out cursor-pointer'>Menu</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in ease-out cursor-pointer'>About Us</li>
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in ease-out cursor-pointer'>Services</li>
-          </ul>
+          </motion.ul>
 
           <div className='relative flex items-center'>
             <MdShoppingBasket className='text-textColor text-3xl cursor-pointer' />
@@ -56,9 +71,31 @@ const Header = () => {
           </div>
 
           <div className='relative'>
-            <motion.img whileTap={{ scale: 0.6 }} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer' src={Avatar} alt="userProfile" onClick={login} />
+            <motion.img whileTap={{ scale: 0.6 }} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' src={user ? user.photoURL : Avatar}
+              // the above src shows that if user is present then its profile will be shown otherwise avatar
+              alt="userProfile" onClick={login} />
+
+            {
+              isMenu && (
+                <motion.div 
+                initial={{opacity:0., scale:0.6}}
+                animate={{opacity:1, scale:1}}
+                opacity={{opacity:1, scale:1}}
+                className='w-40 bg-green-50 rounded-lg shadow-xl flex flex-col absolute top-12 right-0'>
+
+                  {user && user.email === "guptadeeya12@gmail.com" && (
+                    <Link to={"/createItem"}>
+                      <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 ease-in-out'>New Item <MdAdd /> </p>
+                    </Link>
+                  )}
+
+                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 ease-in-out'>Logout <MdLogout /></p>
+                </motion.div>
+              )
+            }
           </div>
         </div>
+
       </div>
       {/* For mobiles */}
       <div className='flex md:hidden w-full h-full'>
