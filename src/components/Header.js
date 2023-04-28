cfd14bf0df7267ab0684465a0d88db028718f956
Deeya <guptadeeya12@gmail.com>
2023-04-28 11:17:53 +0530

    Making state persist on refresh-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,9 +36,20 @@ const Header = () => {
     }
   };
 
+  // function performed after logout button is clicked
+  const logout= () =>{
+    setIsMenu(false)
+    localStorage.clear()
+
+    dispatch({
+      type: actionType.SET_USER,
+      user:null
+    })
+  };
+
   return (
     //p is for padding
-    <header className="fixed z-50 w-screen p-6 px-16 bg-primary">
+    <header className="fixed z-50 w-screen p-4 px-6 md:p-6 md:px-16 bg-primary">
 
       {/* for desktops and tablets  */}
       <div className='hidden md:flex w-full h-full items-center justify-between'>
@@ -50,11 +61,11 @@ const Header = () => {
         </Link>
         <div className='flex items-center gap-8'>
           {/* ml-auto is classname used for marginleft-auto which pushes the items to the right side */}
-          <motion.ul 
-          initial={{opacity: 0, x:200}}
-          animate={{opacity:1, x:0}}
-          exit={{opacity:0, x:200}}
-          className='flex items-center gap-10'>
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className='flex items-center gap-10'>
 
             {/* text-base is 16px */}
             <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in ease-out cursor-pointer'>Home</li>
@@ -77,11 +88,11 @@ const Header = () => {
 
             {
               isMenu && (
-                <motion.div 
-                initial={{opacity:0., scale:0.6}}
-                animate={{opacity:1, scale:1}}
-                opacity={{opacity:1, scale:1}}
-                className='w-40 bg-green-50 rounded-lg shadow-xl flex flex-col absolute top-12 right-0'>
+                <motion.div
+                  initial={{ opacity: 0., scale: 0.6 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  opacity={{ opacity: 1, scale: 1 }}
+                  className='w-40 bg-green-50 rounded-lg shadow-xl flex flex-col absolute top-12 right-0'>
 
                   {user && user.email === "guptadeeya12@gmail.com" && (
                     <Link to={"/createItem"}>
@@ -89,7 +100,7 @@ const Header = () => {
                     </Link>
                   )}
 
-                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 ease-in-out'>Logout <MdLogout /></p>
+                  <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 ease-in-out' onClick={logout}>Logout <MdLogout /></p>
                 </motion.div>
               )
             }
@@ -98,7 +109,44 @@ const Header = () => {
 
       </div>
       {/* For mobiles */}
-      <div className='flex md:hidden w-full h-full'>
+      <div className='flex md:hidden items-center justify-between w-full h-full'>
+        <Link to="/" className='flex items-center gap-2'>
+          {/* Logo link img */}
+          <img src={Logo} className='w-8 object-cover' alt="Logo" />
+          <p className='text-headingColor text-xl font-bold'>Gupta</p>
+        </Link>
+
+        <div className='relative'>
+          <motion.img whileTap={{ scale: 0.6 }} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full' src={user ? user.photoURL : Avatar}
+            // the above src shows that if user is present then its profile will be shown otherwise avatar
+            alt="userProfile" onClick={login} />
+
+          {
+            isMenu && (
+              <motion.div
+                initial={{ opacity: 0., scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                opacity={{ opacity: 1, scale: 1 }}
+                className='w-40 bg-green-50 rounded-lg shadow-xl flex flex-col absolute top-12 right-0'>
+
+                {user && user.email === "guptadeeya12@gmail.com" && (
+                  <Link to={"/createItem"}>
+                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd /> </p>
+                  </Link>
+                )}
+                <ul className='flex flex-col'>
+
+                  {/* text-base is 16px */}
+                  <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-gray-300 px-4 py-2'>Home</li>
+                  <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-gray-300 px-4 py-2'>Menu</li>
+                  <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-gray-300 px-4 py-2'>About Us</li>
+                  <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-gray-300 px-4 py-2'>Services</li>
+                </ul>
+                <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-300 gap-3 cursor-pointer hover:bg-gray-500 transition-all duration-100 ease-in-out text-textColor text-base' onClick={logout}>Logout <MdLogout /></p>
+              </motion.div>
+            )
+          }
+        </div>
       </div>
     </header>
   );
