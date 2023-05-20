ab07516b1955a3765e375f592a38bb91c85f00b5
Deeya <guptadeeya12@gmail.com>
2023-05-20 22:33:36 +0530

    Updating cart total and quantity

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -2,28 +2,29 @@ import React from 'react'
 import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import { motion } from 'framer-motion'
 import { RiRefreshFill } from 'react-icons/ri'
-import { BiMinus, BiPlus } from 'react-icons/bi'
 import { useStateValue } from '../context/StateProvider'
 import { actionType } from '../context/reducer'
+import EmptyCart from "../img/emptyCart.svg"
+import CartItem from './CartItem'
 
 const CartContainer = () => {
 
-    const [{ cartShow }, dispatch] = useStateValue()
+    const [{ cartShow, cartItems, user }, dispatch] = useStateValue()
 
-    const showCart=() =>{
+    const showCart = () => {
         dispatch({
-          type: actionType.SET_CART_SHOW,
-         cartShow: !cartShow,
+            type: actionType.SET_CART_SHOW,
+            cartShow: !cartShow,
         })
-      }
+    }
 
     return (
         <motion.div
-        initial={{opacity:0, x: 200}}
-        animate={{opacity:1, x: 0}}
-        exit={{opacity:0, x: 200}}
-        
-        className='fixed top-0 right-0 w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]'>
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+
+            className='fixed top-0 right-0 w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col z-[101]'>
             <div className='w-full flex items-center  justify-between p-4 cursor-pointer'>
 
                 <motion.div whileTap={{ scale: 0.75 }} onClick={showCart}>
@@ -36,59 +37,62 @@ const CartContainer = () => {
             </div>
 
             {/* bottom section */}
-            <div className='w-full h-full bg-cartBg flex flex-col rounded-t-[2rem]'>
+            {cartItems && cartItems.length > 0 ? (
+                <div className='w-full h-full bg-cartBg flex flex-col rounded-t-[2rem]'>
 
-                {/* cart items section */}
-                <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
+                    {/* cart items section */}
+                    <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
 
-                    {/* cart item */}
-                    <div className='w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2'>
-                        <img src="https://firebasestorage.googleapis.com/v0/b/restaurantsite-473c7.appspot.com/o/Images%2F1683435354134%20-%20i4.png?alt=media&token=5a0887bd-496b-4dd9-81bc-4bec7df947c9" className='w-2p h-20 max-w-[60px] rounded-full object-contain' alt="cart-item" />
+                        {/* cart item */}
+                        {
+                            // a cart item can be empty but its value will not be 0 as there is empty cart
+                            cartItems && cartItems.map(item => (
+                                <CartItem key={item.id} item={item}/>
+                            ))
+                        }
+                    </div>
 
-                        {/* name section */}
-                        <div className='flex flex-col gap-2'>
-                            <p className='text-base text-gray-50'>Sundae</p>
-                            <p className='text-sm block text-gray-300 font-semibold'>$8.5</p>
+                    {/* Cart total section */}
+                    <div className='w-full flex-1 bg-cartTotal flex flex-col items-center justify-evenly px-8 py-2 rounded-t-[2rem]'>
+                        <div className='w-full flex items-center justify-between'>
+                            <p className='text-gray-400 text-lg'>Sub Total</p>
+                            <p className='text-gray-400 text-lg'>$8.5</p>
                         </div>
 
-                        {/* button section */}
-                        <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
-                            <motion.div whileTap={{ scale: 0.75 }}>
-                                <BiMinus className="text-gray-50" />
-                            </motion.div>
-                            <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>1</p>
-                            <motion.div whileTap={{ scale: 0.75 }}>
-                                <BiPlus className="text-gray-50" />
-                            </motion.div>
+                        <div className='w-full flex items-center justify-between'>
+                            <p className='text-gray-400 text-lg'>Delivery</p>
+                            <p className='text-gray-400 text-lg'>$2.5</p>
                         </div>
-                    </div>
 
+                        <div className='w-full border-b border-gray-600 my-3'></div>
+
+                        <div className='w-full flex items-center justify-between'>
+                            <p className='text-gray-200 text-xl font-semibold'>Total</p>
+                            <p className='text-gray-200 text-xl font-semibold'>$11.0</p>
+                        </div>
+
+
+                        {user ? (
+                            <motion.button
+                            whileTap={{ scale: 0.8 }}
+                            type='button'
+                            className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg'>Check Out</motion.button>
+                        ) : (
+                            <motion.button
+                            whileTap={{ scale: 0.8 }}
+                            type='button'
+                            className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg'>Login to check out</motion.button> 
+                        )}
+
+                    </div>
                 </div>
-
-                {/* Cart total section */}
-                <div className='w-full flex-1 bg-cartTotal flex flex-col items-center justify-evenly px-8 py-2 rounded-t-[2rem]'>
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-400 text-lg'>Sub Total</p>
-                        <p className='text-gray-400 text-lg'>$8.5</p>
-                    </div>
-
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-400 text-lg'>Delivery</p>
-                        <p className='text-gray-400 text-lg'>$2.5</p>
-                    </div>
-
-                    <div className='w-full border-b border-gray-600 my-3'></div>
-
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-200 text-xl font-semibold'>Total</p>
-                        <p className='text-gray-200 text-xl font-semibold'>$11.0</p>
-                    </div>
-
-                    <motion.button whileTap={{ scale: 0.75 }} type='button' className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-500 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg '>
-                        Check Out
-                    </motion.button>
+            ) : (
+                <div className='w-full h-full flex flex-col items-center justify-center gap-6'>
+                    <img src={EmptyCart} className='w-300' alt="empty cart" />
+                    <p className='text-xl text-textColor font-semibold'>Add some items to your cart</p>
                 </div>
-            </div>
+            )}
+
         </motion.div>
     )
 }
--- src/components/RowContainer.js
+++ src/components/RowContainer.js
@@ -1,29 +1,54 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdShoppingBasket } from 'react-icons/md'
 import { motion } from 'framer-motion'
 import NotFound from '../img/NotFound.svg'
+import { useStateValue } from '../context/StateProvider'
+import { actionType } from '../context/reducer'
 
 const RowContainer = ({ flag, data, scrollValue }) => {
     const RowContainer = useRef()
-    console.log(data);
+    // console.log(data);
+
+    const [items, setItems] = useState([])
+
+    /// eslint-disable-next-line
+    const [{ cartItems }, dispatch] = useStateValue();
+
+    const addToCart = () => {
+        // console.log(item);
+
+        dispatch({
+            type : actionType.SET_CARTITEMS,
+            // destructuring cart items
+            cartItems : items,
+        });
+        // to prevent the cart from updating to zero on refreshing we;ll apply below code
+        localStorage.setItem("cartItems",JSON.stringify(items));
+    };
+
     useEffect(() => {
         RowContainer.current.scrollLeft += scrollValue;
     }, [scrollValue])
+
+useEffect( ()=>{
+    addToCart()
+}, [items])
+
     return (
 
         // haan aari h accha mtlb mujhe phle sare storage m add krne pdenge firebase m??
-        
+
         // below classname is a dynamic string
         <div ref={RowContainer} className={`w-full flex my-10 gap-4 scroll-smooth bg-rowBg ${flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden flex-wrap justify-center h-200'}`}>
- 
+
             {data && data.length > 0 ? data.map(item => (
                 <div key={item?.id} className='w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px] my-12 backdrop-blur-lg bg-cardOverlay rounded-lg py-2 px-4 hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'>
                     <div className='w-full flex items-center justify-between'>
                         <motion.div whileHover={{ scale: 1.2 }} className='w-40 h-40 -mt-8 drop-shadow-2xl'>
-                        <img src={item?.imageURL} alt="" className="w-full h-full object-contain" />
+                            <img src={item?.imageURL} alt="" className="w-full h-full object-contain" />
                         </motion.div>
 
-                        <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-400 flex items-center justify-center cursor-pointer hover:shadow-md'>
+                        <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-full bg-red-400 flex items-center justify-center cursor-pointer hover:shadow-md' onClick={() => setItems([...cartItems, item])}>
                             <MdShoppingBasket className='text-white' />
                         </motion.div>
                     </div>
@@ -42,11 +67,11 @@ const RowContainer = ({ flag, data, scrollValue }) => {
                         </div>
                     </div>
                 </div>
-            )) : 
-            <div className='w-full flex flex-col items-center justify-center'>
-            <img src={NotFound} className='h-300' alt='Not found'/>
-            <p className='text-xl text-headingColor font-semibold my-3'>Items Not Available</p>
-            </div>
+            )) :
+                <div className='w-full flex flex-col items-center justify-center'>
+                    <img src={NotFound} className='h-300' alt='Not found' />
+                    <p className='text-xl text-headingColor font-semibold my-3'>Items Not Available</p>
+                </div>
             }
         </div>
     )
